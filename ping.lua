1bff11dd70d50a654550fdbaacc578d55420879c
Nickjgniklu <Nickjgniklu@gmail.com>
2018-11-10 14:16:57 -0700

    measures and prints 10 times per second to the console

--- ping.lua
+++ ping.lua
@@ -3,21 +3,24 @@
 --to use this senore we send a small pulse
 --then on the smae pin wait to hear a ulse back and measure the length of the pulse
 
---send small pulse
+-- pin to use
 pin=7
+-- for logging pulse time
 start=0
 fin=0
+
+-- stores most recent distance
+distance=-1
 
 -- level is current state of pin 
 --when is timestamp
 --count --- number of times triggerd
-function event(level,when,count)
+function pulseEvent(level,when,count)
     if level == gpio.HIGH then
         start=when
     elseif level == gpio.LOW then
         fin =when
-        print("The distance measured in inches is")
-        print((fin-start)/74/2)
+        distance=(fin-start)/74/2
     end
 
 end
@@ -28,12 +31,22 @@ function sendPulse()
     gpio.write(pin,gpio.HIGH)
     tmr.delay(5)
 end
-
-sendPulse()
--- after the out pules there are 750uS until we have 
---to be ready for the input pulse to come in
--- listen for pulse
-
-gpio.mode(pin,gpio.INT)--170S for this
-gpio.trig(pin,"both", event)--??second dont do anon function takes too long!
-
+function measureDistance()
+    sendPulse()
+    -- after the out pules there are 750uS until we have 
+    --to be ready for the input pulse to come in
+    -- listen for pulse
+    gpio.mode(pin,gpio.INT)
+    gpio.trig(pin,"both", pulseEvent)--??second dont do anon function takes too long!
+end
+function printDistance()
+measureDistance()
+-- this takes time to update we must wait
+-- print 50 milliseconds later
+tmr.alarm(1,50,tmr.ALARM_SINGLE, function ()
+        print("Distance="..distance)
+    end
+)
+end
+--schedule to print the distance 10 time per second
+tmr.alarm(2,100,tmr.ALARM_AUTO,printDistance)
