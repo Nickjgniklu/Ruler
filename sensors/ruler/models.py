8d4769e13b79c31455d6904d01ba2823dc7dc479
Nickjgniklu <Nickjgniklu@gmail.com>
2018-11-10 17:11:02 -0700

    simple api to submit a distance via GET and have it saved into the database

--- sensors/ruler/models.py
+++ sensors/ruler/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Measurement(models.Model):
+    name= models.CharField(max_length=30)
+    distance = models.CharField(max_length=30)
+    date_posted = models.DateTimeField('Posted Date')
+
+    def __str__(self):
+        return f"{self.name}({self.id})"
--- sensors/ruler/views.py
+++ sensors/ruler/views.py
@@ -1,8 +1,20 @@
 from django.template import loader
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Measurement
 # Create your views here.
 def index(request):
-    context={"test":3}
+    context={"distance":Measurement.objects.all()[0].distance}
     template=loader.get_template('ruler/index.html')
     return HttpResponse(template.render(context,request))
+def fake(request):
+    if(len(Measurement.objects.all())<1):
+        m= Measurement(name="inches",distance="100",date_posted=timezone.now())
+        m.save()
+    else:
+       m= Measurement.objects.all()[0]
+       m.distance = request.GET["distance"]
+       m.save()
+
+    return redirect('index')
