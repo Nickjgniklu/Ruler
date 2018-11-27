c8f6ed7689915d046d8fb21d7b688c2f498273c9
Nickjgniklu <Nickjgniklu@gmail.com>
2018-11-27 12:41:36 -0700

    adjusted fontsize

--- sensors/ruler/static/ruler/UltraApp.js
+++ sensors/ruler/static/ruler/UltraApp.js
@@ -2,12 +2,12 @@
     var background=paper.rect(0,0,"100%","100%");
     background.attr("fill","#444");
     background.attr("stroke","#000");
-    var circle = paper.circle(getWidth()/2,getHeight()/2,30);
+    var circle = paper.circle(getWidth()/2,getHeight()/2,60);
     circle.attr("fill","#159");
     circle.glow();
-
+//trying to adjust circle fro mobile
     var text = paper.text(getWidth()/2,getHeight()/2,"Distance\n"+"No Data");
-    text.attr({ "font-size": 16, "font-family": "\"Courier New\", Courier, monospace" });
+    text.attr({ "font-size": 40, "font-family": "\"Courier New\", Courier, monospace" });
     makeAPIcall()//make the ifrst call the rest are called on time intervals
 
    // createXArcs(10);
@@ -31,7 +31,7 @@ function getWidth(){
         //place the arc in the middle of the screen
         let arc = paper.path(["M",width/2-length,height/2,"Q",width/2,height/2-depth,width/2+length,height/2,"Q",width/2,height/2-depth+20,width/2-length,height/2])
         // move the arc 200 pixel up double it in size and then delete it
-        let tpath=Raphael.transformPath(arc.attr("path"),'T0,-200,S2');
+        let tpath=Raphael.transformPath(arc.attr("path"),'T0,-400,S2');
         arc.animate({path:tpath},1000,function(){
                 arc.remove()
 
